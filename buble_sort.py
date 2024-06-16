d4e04d3149c863700cdf220d70e2b06699e9e8d3
omurbekabakirov <abkirovomaa@gmail.com>
2024-06-16 20:45:51 +0600

    hw6

--- buble_sort.py
+++ buble_sort.py
@@ -1,15 +1,12 @@
 from random import randint
-
-
-def bubble_sort():
+def bubble_sort(lst):
     lst = [randint(1, 1000) for _ in range(5)]
-    print(f"The list{lst}")
     n = len(lst)
     for i in range(n):
-        for j in range(0,   n - 1 - i):
+        for j in range(0, n - i - 1):
             if lst[j] > lst[j + 1]:
                 lst[j], lst[j + 1] = lst[j + 1], lst[j]
-                print(lst)
 
-
-bubble_sort()
+    return lst
+random_numbers = bubble_sort([])
+print("Sorted List:", random_numbers)
